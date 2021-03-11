f3f3af40e09f157af604161c711c0b0d8c2a2fde
Giuseppe Barbieri <elect86@gmail.com>
2021-03-11 08:02:58 +0100

    up

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 import java.io.ByteArrayOutputStream
 
-fun gitDescribe(): String {
-    val stdout = ByteArrayOutputStream()
-    rootProject.exec {
-        commandLine("git", "describe", "--tags")
-        standardOutput = stdout
+val gitDescribe: String by lazy {
+        val stdout = ByteArrayOutputStream()
+        rootProject.exec {
+            commandLine("git", "describe", "--tags")
+            standardOutput = stdout
+        }
+        stdout.toString().trim()
     }
-    return stdout.toString().trim()
-}
 
 
 subprojects {
@@ -16,18 +16,19 @@ subprojects {
     apply(plugin = "maven-publish")
 
     group = "kotlin.graphics.platform"
-    version = "0.1.9"
+    //    version = "0.1.9"
+    version = gitDescribe
 
     extensions.configure(PublishingExtension::class) {
         publications.create<MavenPublication>("maven") {
             from(components["javaPlatform"])
         }
         repositories.maven {
-            name = "scijava"
-            url = uri("https://maven.scijava.org/content/repositories/releases")
+            //            name = "scijava"
+            //            url = uri("https://maven.scijava.org/content/repositories/releases")
+            name = "repsy"
+            url = uri("https://repo.repsy.io/mvn/elect/kx")
             credentials(PasswordCredentials::class)
         }
     }
 }
-
-println(gitDescribe())
