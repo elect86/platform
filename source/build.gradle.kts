d46438d073a46be6ef41e762e35a95fb03f71ed1
Giuseppe Barbieri <elect86@gmail.com>
2021-03-22 18:39:06 +0100

    v0.1.9+31

--- source/build.gradle.kts
+++ source/build.gradle.kts
@@ -1,4 +1,3 @@
-
 // allow the definition of dependencies to other platforms like the Spring Boot BOM
 extensions.getByName<JavaPlatformExtension>("javaPlatform").allowDependencies()
 
@@ -7,19 +6,21 @@ dependencies {
     api(platform("org.lwjgl:lwjgl-bom:3.2.3"))
 
     constraints {
-//        api("org.apache.juneau:juneau-marshall:8.2.0")
-//        api(platform(":kx"))
+        //        api("org.apache.juneau:juneau-marshall:8.2.0")
+        //        api(platform(":kx"))
 
-        for((art, vers) in mapOf(
+        for ((art, vers) in mapOf(
             "unsigned" to "3.2.9+24",
-            "kool" to "0.9.0+20"))
-                 api("kotlin.graphics:$art:$vers")
-//
-//        api("$kx:unsigned:")
-//        api("$kx:kool:0.9.0+20")
-//        api("$kx:glm:8dae70c1")
-//        api("$kx:gli:12cd3342")
-//        api("$kx:gln:9eae85c8")
-//        api("$kx:vkk:9d1cd859")
+            "kool" to "0.9.0+20",
+            "glm" to "0.9.9.1-3+02"
+                                 ))
+            api("kotlin.graphics:$art:$vers")
+        //
+        //        api("$kx:unsigned:")
+        //        api("$kx:kool:0.9.0+20")
+        //        api("$kx:glm:8dae70c1")
+        //        api("$kx:gli:12cd3342")
+        //        api("$kx:gln:9eae85c8")
+        //        api("$kx:vkk:9d1cd859")
     }
 }
