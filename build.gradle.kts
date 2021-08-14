816893121ca405df8c3897ed9c42d969954ad12d
Giuseppe Barbieri <elect86@gmail.com>
2021-08-14 08:04:37 +0200

    0.2.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,43 @@
+import magik.SoftwareComponent.javaPlatform
+import magik.alsoSnapshot
+import magik.github
 
 plugins {
-    id("kx.snapshot") version "0.0.5"
+//    id("kx.snapshot") version "0.0.5"
 //    id("com.google.cloud.artifactregistry.gradle-plugin") version "2.1.1"
+
+    id("elect86.magik") version "0.0.8" apply false
 }
 
-version = "0.2.8+46" // for ::bump
+version = "0.2.9" // for magik
 
 subprojects {
 
     apply(plugin = "java-platform")
     apply(plugin = "maven-publish")
+    apply(plugin = "elect86.magik")
 
     group = "kotlin.graphics.platform"
     version = rootProject.version
 
-    extensions.configure(PublishingExtension::class) {
+    extensions.configure<PublishingExtension> {
         publications.create<MavenPublication>("maven") {
             from(components["javaPlatform"])
             suppressPomMetadataWarningsFor("apiElements")
-        }
-        repositories.maven {
-            url = uri("$rootDir/../mary")
-            //            name = "scijava"
-            //            url = uri("https://maven.scijava.org/content/repositories/releases")
-            //            name = "repsy"
-            //            url = uri("https://repo.repsy.io/mvn/elect/kx")
-            //            name = "aws"
-            //            url = uri("https://kx-995066660206.d.codeartifact.eu-central-1.amazonaws.com/maven/mary/")
-            //            credentials(PasswordCredentials::class)
-            //            url = uri("artifactregistry://europe-west6-maven.pkg.dev/galvanized-case-306920/kx")
+        }.alsoSnapshot(component = javaPlatform)
+//        repositories.maven {
+//            url = uri("$rootDir/../mary")
+//            //            name = "scijava"
+//            //            url = uri("https://maven.scijava.org/content/repositories/releases")
+//            //            name = "repsy"
+//            //            url = uri("https://repo.repsy.io/mvn/elect/kx")
+//            //            name = "aws"
+//            //            url = uri("https://kx-995066660206.d.codeartifact.eu-central-1.amazonaws.com/maven/mary/")
+//            //            credentials(PasswordCredentials::class)
+//            //            url = uri("artifactregistry://europe-west6-maven.pkg.dev/galvanized-case-306920/kx")
+//        }
+        repositories.github {
+            domain = "kotlin-graphics/mary"
         }
     }
 }
--- source/build.gradle.kts
+++ source/build.gradle.kts
@@ -5,30 +5,30 @@ dependencies {
 
     api(platform("org.lwjgl:lwjgl-bom:3.2.3"))
 
-    constraints {
-        //        api("org.apache.juneau:juneau-marshall:8.2.0")
-        //        api(platform(":kx"))
-
-        val uno = "0.7.9+35"
-        for ((art, vers) in mapOf(
-            "unsigned" to "3.2.9+31",
-            "kool" to "0.9.0+23",
-            "glm" to "0.9.9.1-3+23",
-            "gli" to "0.8.3.0-16+22",
-            "gln" to "0.5.2+22",
-            "vkk" to "0.3.2+36",
-            "uno" to uno,
-            "uno-awt" to uno,
-            "uno-core" to uno,
-            "uno-vk" to uno,
-                                 ))
-            api("kotlin.graphics:$art:$vers")
-        //
-        //        api("$kx:unsigned:")
-        //        api("$kx:kool:0.9.0+20")
-        //        api("$kx:glm:8dae70c1")
-        //        api("$kx:gli:12cd3342")
-        //        api("$kx:gln:9eae85c8")
-        //        api("$kx:vkk:9d1cd859")
-    }
+//    constraints {
+//        //        api("org.apache.juneau:juneau-marshall:8.2.0")
+//        //        api(platform(":kx"))
+//
+//        val uno = "0.7.9+35"
+//        for ((art, vers) in mapOf(
+//            "unsigned" to "3.2.9+31",
+//            "kool" to "0.9.0+23",
+//            "glm" to "0.9.9.1-3+23",
+//            "gli" to "0.8.3.0-16+22",
+//            "gln" to "0.5.2+22",
+//            "vkk" to "0.3.2+36",
+//            "uno" to uno,
+//            "uno-awt" to uno,
+//            "uno-core" to uno,
+//            "uno-vk" to uno,
+//                                 ))
+//            api("kotlin.graphics:$art:$vers")
+//        //
+//        //        api("$kx:unsigned:")
+//        //        api("$kx:kool:0.9.0+20")
+//        //        api("$kx:glm:8dae70c1")
+//        //        api("$kx:gli:12cd3342")
+//        //        api("$kx:gln:9eae85c8")
+//        //        api("$kx:vkk:9d1cd859")
+//    }
 }
